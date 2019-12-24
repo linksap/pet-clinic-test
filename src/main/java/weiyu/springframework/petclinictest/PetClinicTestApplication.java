3e0db2110f1e93bb04a1ab608565ecc0c2a69ce4
weiyu liu <weiyuliu@weiyus-MacBook-Pro.local>
2019-12-24 02:53:46 -0500

    Initial Project

---
+++ src/main/java/weiyu/springframework/petclinictest/PetClinicTestApplication.java
@@ -0,0 +1,13 @@
+package weiyu.springframework.petclinictest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/weiyu/springframework/petclinictest/PetClinicTestApplicationTests.java
@@ -0,0 +1,13 @@
+package weiyu.springframework.petclinictest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
